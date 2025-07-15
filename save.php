1aec2298e4dc7b4642a6ec2ad3d2ee6a64a323d6
Prashant Kumar <prashantkumarbgs22@gmail.com>
2025-07-15 21:13:29 +0530

    Update save.php

--- save.php
+++ save.php
@@ -1,12 +1,35 @@
 <?php
 session_start();
+include 'db.php';
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $_SESSION['name'] = $_POST['name'] ?? '';
-    $_SESSION['email'] = $_POST['email'] ?? '';
-    $_SESSION['age'] = $_POST['age'] ?? '';
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $name = mysqli_real_escape_string($conn, $_POST['name'] ?? '');
+    $email = mysqli_real_escape_string($conn, $_POST['email'] ?? '');
+    $age = (int)($_POST['age'] ?? 0);
+    $id = isset($_POST['id']) && $_POST['id'] !== '' ? (int)$_POST['id'] : 0;
+
+    $_SESSION['name'] = $name;
+    $_SESSION['email'] = $email;
+    $_SESSION['age'] = $age;
+
+    if ($id > 0) {
+        $query = "UPDATE users SET name='$name', email='$email', age=$age WHERE id=$id";
+        $result = mysqli_query($conn, $query);
+        if (!$result) {
+            die("Update Error: " . mysqli_error($conn));
+        }
+    } else {
+        $query = "INSERT INTO users (name, email, age) VALUES ('$name', '$email', $age)";
+        $result = mysqli_query($conn, $query);
+        if (!$result) {
+            die(" Insert Error: " . mysqli_error($conn));
+        }
+        $id = mysqli_insert_id($conn);
+    }
+
+    $_SESSION['id'] = $id;
+
+    header("Location: view.php");
+    exit;
 }
-
-header("Location: view.php");
-exit;
 ?>
