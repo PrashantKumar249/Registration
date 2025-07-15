54de2a8a9e8485a7994f8993ed53a4f5459e2fb3
Prashant Kumar <prashantkumarbgs22@gmail.com>
2025-07-15 21:21:19 +0530

    Update save.php

--- save.php
+++ save.php
@@ -3,15 +3,18 @@ session_start();
 include 'db.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $name = ($conn, $_POST['name'] ?? '');
-    $email =($conn, $_POST['email'] ?? '');
+    
+    $name = $_POST['name'] ?? '';
+    $email = $_POST['email'] ?? '';
     $age = (int)($_POST['age'] ?? 0);
     $id = isset($_POST['id']) && $_POST['id'] !== '' ? (int)$_POST['id'] : 0;
 
+    
     $_SESSION['name'] = $name;
     $_SESSION['email'] = $email;
     $_SESSION['age'] = $age;
 
+   
     if ($id > 0) {
         $query = "UPDATE users SET name='$name', email='$email', age=$age WHERE id=$id";
         $result = mysqli_query($conn, $query);
@@ -22,7 +25,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $query = "INSERT INTO users (name, email, age) VALUES ('$name', '$email', $age)";
         $result = mysqli_query($conn, $query);
         if (!$result) {
-            die(" Insert Error: " . mysqli_error($conn));
+            die("Insert Error: " . mysqli_error($conn));
         }
         $id = mysqli_insert_id($conn);
     }
